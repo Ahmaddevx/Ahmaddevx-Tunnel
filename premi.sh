c2501ebc905fcaa6ab5ab40c5d4a8fbc7ca5c90f
Ahmaddevx <ahmaddevx@gmail.com>
2024-12-06 03:24:07 +0700

    Update premi.sh

--- premi.sh
+++ premi.sh
@@ -532,7 +532,7 @@ print_success "Password SSH"
 function udp_mini(){
 clear
 print_install "Memasang Service Limit Quota"
-wget raw.githubusercontent.com/Ahmaddevx-Tunnel/main/limit/limit.sh && chmod +x limit.sh && ./limit.sh
+wget https://raw.githubusercontent.com/Ahmaddevx/Ahmaddevx-Tunnel/main/limit/limit.sh && chmod +x limit.sh && ./limit.sh
 
 cd
 wget -q -O /usr/bin/limit-ip "${REPO}limit/limit-ip"
